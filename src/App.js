65a1dfabfca436181e474389ff317a773487b112
Pankaj yadav <abhi7868090@gmail.com>
2023-12-15 22:24:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-import ClinicPage from "./components/ClinicPage";
 import Home from "./components/Home";
-import Services from "./components/Services";
 
 function App() {
   return (
     <div className=" bg-blue-600">
       <Home />
 
- 
     </div>
   );
 }
